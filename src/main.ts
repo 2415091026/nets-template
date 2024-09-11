250985839ce9c40bc083a6477c989a5de8177596
2415091026@qq.com <2415091026@qq.com>
2024-09-11 15:13:38 +0800

    首次提交

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './core/filter/http-exception/http-exception.filter';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { TransformInterceptor } from './core/interceptor/transform/transform.interceptor';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new TransformInterceptor());
+  app.useGlobalPipes(new ValidationPipe());
+  // 设置swagger文档
+  const config = new DocumentBuilder()
+    .setTitle('管理后台')
+    .setDescription('管理后台接口文档')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(3000);
+  console.log(
+    `服务请访问http://localhost:3000`,
+    `接口文档请访问http://localhost:3000/docs`,
+  );
+}
+bootstrap();
